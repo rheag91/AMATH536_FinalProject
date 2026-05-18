b6df2cb30378beddce9c6940b275b62505eb19e3
rheag91 <rhea.m.grover@gmail.com>
2026-05-18 09:48:13 -0700

    Adding Lange simulations including fixed random number generation

---
+++ ecDNA_Simulations/Anfangsverteilung.h
@@ -0,0 +1,21 @@
+/*
+ *  Anfangsverteilung.h
+ *  
+ *
+ *  Created by Benjamin Werner on 10/31/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+# include <iostream>
+# include <cmath>
+# include <ctime>
+# include <cstdlib>
+# include <cstdio>
+# include <vector>
+# include <fstream>
+#include <math.h>
+
+using std::vector;
+using std::cout;
+using std::cin;
